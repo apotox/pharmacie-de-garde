17e6c0a64cdfe4e5cabdc8c3af0ae7e2f79c0469
saphidev <saphidev@gmail.com>
2021-08-07 14:03:23 +0100

    fixs

--- app/src/main/java/com/saphidev/pharmaciedegarde/data/configuration.kt
+++ app/src/main/java/com/saphidev/pharmaciedegarde/data/configuration.kt
@@ -1,8 +1,8 @@
 package com.saphidev.pharmaciedegarde.data
 
 data class configuration (
-    var cityId: Int  = 0,
-    var daira: String = "",
-    var commune : String = "",
-    var city : String = "",
+    var cityId: Int  = 118,
+    var daira: String = "Ain Touta",
+    var commune : String = "Ain Touta",
+    var city : String = "Batna",
  )
